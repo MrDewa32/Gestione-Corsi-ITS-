be233d9eaed7e259a92dc4b598049bd5f5089dd5
NovaTrigger <83513680+LokyV1@users.noreply.github.com>
2025-11-29 01:11:21 +0100

    feat: Implement initial UI structure including global styles, app layout, and a new navbar component.

--- FrontEnd/ITS/src/app/navbar/navbar.ts
+++ FrontEnd/ITS/src/app/navbar/navbar.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatMenuModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
 
+  toggleMenu() {
+    this.trigger.toggleMenu();
+  }
 }
