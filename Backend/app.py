a57088fdb039670469ce96869d793a44c1198d91
MrDewa32 <adigiuseppe@studenti.apuliadigitalmaker.it>
2025-11-29 17:00:21 +0100

    aggiuto il collegamento al database

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import pymongo
+
+client = pymongo.MongoClient("localhost", 27017)
+db = client["Gestione-Corsi-ITS"]
 
 app = Flask(__name__)
 
@@ -6,3 +10,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/test-mongo")
+def test_mongo():
+    return client.list_database_names()
