482dabf54356cec033da82886a633c2169c0ec11
NovaTrigger <83513680+LokyV1@users.noreply.github.com>
2025-11-30 20:36:12 +0100

    feat: add Snackbar component and update Angular Material and Tailwind CSS dependencies.

--- FrontEnd/ITS/src/app/app.routes.ts
+++ FrontEnd/ITS/src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { Corso } from './corso/corso';
 
 export const routes: Routes = [
     { path: '', component: Homepage, title: 'Home' },
-    { path: 'corsi', component: Corso, title: 'Corsi' }
+    { path: 'corsi', component: Corso, title: 'Corsi' },
 ];
