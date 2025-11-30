e7eedeff5a4fb23c986693b8383bd7d71fffb91d
NovaTrigger <83513680+LokyV1@users.noreply.github.com>
2025-11-30 12:53:29 +0100

    inizio a seguire il wireframe di lucrezia, non badate al bacground dopo lo cambio

--- FrontEnd/ITS/src/app/navbar/navbar.ts
+++ FrontEnd/ITS/src/app/navbar/navbar.ts
@@ -5,13 +5,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatMenuModule, RouterModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatMenuModule, RouterModule, MatFormFieldModule, MatInputModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
