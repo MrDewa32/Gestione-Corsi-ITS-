02a246ba0e684614019fce360ea04f7d971b3625
Lux2030 <luca.dentico2003@gmail.com>
2025-12-01 14:25:46 +0100

    Update app.routes.ts

--- FrontEnd/ITS/src/app/app.routes.ts
+++ FrontEnd/ITS/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { Corso } from './corso/corso';
 import { FormRegistrazioneComponent } from './form-registrazione/form-registrazione';
+import { FormCompilazione } from './form-compilazione/form-compilazione';
 
 export const routes: Routes = [
     { path: '', component: Homepage, title: 'Home' },
     { path: 'corsi', component: Corso, title: 'Corsi' },
     { path: 'registrazione', component: FormRegistrazioneComponent, title: 'Registrazione' },
+    { path: 'login', component: FormCompilazione, title: 'Accedi' }, // 👈 Aggiunto
 ];
+
