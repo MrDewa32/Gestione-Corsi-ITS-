326f3c556a5640d69855fd203124fdd8cec56b02
NovaTrigger <83513680+LokyV1@users.noreply.github.com>
2025-12-01 21:00:24 +0100

    Risolti tutti i problemi per ora

--- FrontEnd/ITS/src/app/form-compilazione/form-compilazione.ts
+++ FrontEnd/ITS/src/app/form-compilazione/form-compilazione.ts
@@ -1,35 +1,16 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form-compilazione',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,],
   templateUrl: './form-compilazione.html',
   styleUrls: ['./form-compilazione.css'],
 })
 export class FormCompilazione {
+  
 
-  loginForm: FormGroup;
-
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    });
-  }
-
-  accedi() {
-    if (this.loginForm.valid) {
-
-      console.log("Dati inviati:", this.loginForm.value);
-
-      // 👇 Navigazione dopo login riuscito
-      this.router.navigate(['/corsi']); // <-- cambia percorso se vuoi
-
-    } else {
-      console.log("Form non valido");
-    }
-  }
 }
