055d8bd964633fd6d5200374f20274e8f5b54c09
LokyV1 <luigilok24@gmail.com>
2025-12-01 14:42:02 +0100

    Implementato il form per creare un corso

--- FrontEnd/ITS/src/app/app.routes.ts
+++ FrontEnd/ITS/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { Corso } from './corso/corso';
 import { FormCompilazione } from './form-compilazione/form-compilazione';
+import { Creacorso } from './creacorso/creacorso';
 
 export const routes: Routes = [
     { path: '', component: Homepage, title: 'Home' },
     { path: 'corsi', component: Corso, title: 'Corsi' },
-    { path: 'login', component: FormCompilazione, title: 'Accedi' }, // 👈 Aggiunto
+    { path: 'login', component: FormCompilazione, title: 'Accedi' },
+    { path: 'creacorso', component: Creacorso, title: 'Crea Corso' },
 ];
