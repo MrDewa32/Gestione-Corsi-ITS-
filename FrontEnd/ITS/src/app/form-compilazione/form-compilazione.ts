17a2f2cb70197cf6b628963668df3d3c012c7b66
amicogit <amicog806@gmail.com>
2025-12-01 11:55:40 +0100

    ok

---
+++ FrontEnd/ITS/src/app/form-compilazione/form-compilazione.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-compilazione',
+  imports: [],
+  templateUrl: './form-compilazione.html',
+  styleUrl: './form-compilazione.css',
+})
+export class FormCompilazione {
+
+}
