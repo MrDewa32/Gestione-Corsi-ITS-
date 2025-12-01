4bb19ffb80bfe76d5875f2f50543e8805b0f49e5
amicogit <amicog806@gmail.com>
2025-12-01 12:35:02 +0100

    login1

--- FrontEnd/ITS/src/app/app.routes.ts
+++ FrontEnd/ITS/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { Corso } from './corso/corso';
+import { FormCompilazione } from './form-compilazione/form-compilazione';
 
 export const routes: Routes = [
     { path: '', component: Homepage, title: 'Home' },
     { path: 'corsi', component: Corso, title: 'Corsi' },
+    { path: 'login', component: FormCompilazione, title: 'Accedi' }, // 👈 Aggiunto
 ];
