e8425aa9d3d9c0411ca9c7afd0db994cbedb99f2
LokyV1 <luigilok24@gmail.com>
2025-12-02 12:38:56 +0100

    riparato il form di login, adesso ha senso con il resto del progetto

--- FrontEnd/ITS/src/app/form-compilazione/form-compilazione.ts
+++ FrontEnd/ITS/src/app/form-compilazione/form-compilazione.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-form-compilazione',
   standalone: true,
-  imports: [ReactiveFormsModule,],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './form-compilazione.html',
   styleUrls: ['./form-compilazione.css'],
 })
 export class FormCompilazione {
+  private fb = inject(FormBuilder);
   
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
 
+  hide = true;
+
+  clickEvent(event: MouseEvent) {
+    this.hide = !this.hide;
+    event.stopPropagation();
+  }
 }
