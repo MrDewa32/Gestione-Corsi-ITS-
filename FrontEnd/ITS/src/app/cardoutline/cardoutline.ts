f68a39383f6e392a227039eebc80c0e779000dc9
LokyV1 <luigilok24@gmail.com>
2025-12-02 11:48:04 +0100

    Aggiustato più o meno il dialog di lucrezia

--- FrontEnd/ITS/src/app/cardoutline/cardoutline.ts
+++ FrontEnd/ITS/src/app/cardoutline/cardoutline.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, input, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalDeleteCourseComponent } from '../modal-eliminacorso/modal-eliminacorso';
 
 @Component({
   selector: 'app-cardoutline',
@@ -11,4 +13,13 @@ import { MatButtonModule } from '@angular/material/button';
 export class Cardoutline {
   titolo = input<string>('');
   descrizione = input<string>('');
+  
+  private dialog = inject(MatDialog);
+
+  openDeleteModal() {
+    this.dialog.open(ModalDeleteCourseComponent, {
+      width: '500px',
+      data: { titolo: this.titolo() }
+    });
+  }
 }
